8c8349eceea6848e5306cfacc849065b6a5b76bd
Joevan Duhaney <Joevanduhaney101@hotmail.com>
2024-05-05 07:59:02 -0700

    Add project files.

---
+++ NettlyWebAppMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NettlyWebAppMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
